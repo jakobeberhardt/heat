8c3ab73ecc66a974a6f80cab838f4f0b0118f238
LNJgit <loriennasarre@gmail.com>
2023-11-06 18:03:52 +0100

    CUDA fixes 3

--- part4/kernels.cu
+++ part4/kernels.cu
@@ -6,10 +6,10 @@ __global__ void gpu_Heat (float *h, float *g, int N) {
 	int i = blockIdx.y* blockDim.y + threadIdx.y;
 	int j = blockIdx.x * blockDim.x + threadIdx.x;
 
-	if (i >=1 && i < N -1 && j>=1 && j<= N-1){
-		g[i*N+j]= 0.25 * (h[ i*N     + (j-1) ]+  // left
-					     h[ i*N     + (j+1) ]+  // right
-				             h[ (i-1)*N+ j     ]+  // top
-				             h[ (i+1)*N + j     ]); // bottom
+	if (i >0 && i < N -1 && j> 0 && j< N-1){
+		h[i*N+j]= 0.25 * (g[ i*N + (j-1) ]+  // left
+					     g[ i*N + (j+1) ]+  // right
+				         g[ (i-1)*N+ j ]+  // top
+				         g[ (i+1)*N + j]); // bottom
 	}
 }
